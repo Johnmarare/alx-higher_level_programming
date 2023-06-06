5428d6ae7b73affa99ab6e53eb071d76f94bf7f3
Johnmarare <mararejohn@gmail.com>
2023-06-06 19:28:29 +0000

    Bytecoe -> python#2

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def magic_calculations(a, b, c):
+    """match bytecode provided by alx"""
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
