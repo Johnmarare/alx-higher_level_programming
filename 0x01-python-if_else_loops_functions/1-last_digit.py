35281861c3f99605d14404bb56835a6a48bbbba3
Johnmarare <mararejohn@gmail.com>
2023-06-06 15:25:52 +0000

    prints last digit stored in variable number

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last = abs(number) % 10
+
+if number <= 0:
+    last = last * -1
+
+if last > 5:
+    msg = "greater than 5"
+elif last == 0:
+    msg = "0"
+else:
+    msg = "less than 6 and not 0"
+
+print("Last digit 0f {:d} is {:d} and is {:s}".format(number, last, msg))
