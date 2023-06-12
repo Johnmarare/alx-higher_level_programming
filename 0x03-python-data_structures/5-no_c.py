ce3242f47651303036d63e2a15a5ec18bf2e3d6c
Johnmarare <mararejohn@gmail.com>
2023-06-12 19:14:11 +0000

    removes all xters

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def no_c(my_string):
+    result = ''
+    for letter in my_string:
+        if letter == 'c' or letter == 'c':
+            continue
+
+        result = result + letter
+
+    return result
