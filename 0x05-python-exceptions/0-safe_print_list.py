da00c62771daa5757023b86333359365a65b8215
Johnmarare <mararejohn@gmail.com>
2023-06-27 02:58:37 +0000

    correction my_list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         count = 0
         for i in my_list:
-            print(i, end=' ')
+            print(i, end='')
             count += 1
             if count == x:
                 break
