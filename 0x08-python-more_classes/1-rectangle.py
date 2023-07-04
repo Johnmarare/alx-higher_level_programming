ff5219331da5a696b2917238bc1b3a915b0753a4
Johnmarare <mararejohn@gmail.com>
2023-07-04 09:23:08 +0000

    Real definition of a triangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+# 1-rectangle.py
+"""Defines a Rectangle class based on 0-rectangle.py"""
+
+
+class Rectangle:
+    """This represents a rectangle"""
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Gets the width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """sets the width of the rectangle"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Gets the height of the rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """sets the height of the rectangle"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
