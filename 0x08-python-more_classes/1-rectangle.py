99ef75a9402cd0e2619b76ab510f87327cbff495
Johnmarare <mararejohn@gmail.com>
2023-07-04 09:33:25 +0000

    fix: correcting refrenced parameter passed by the setter method

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -39,6 +39,6 @@ class Rectangle:
         """sets the height of the rectangle"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
