99f52957f73ca3900dd26e0a8982e0de9b332e1a
Johnmarare <mararejohn@gmail.com>
2023-07-10 11:13:52 +0000

    finding the error

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 # 1-my_list.py
-"""Class that shows inheritance"""
+"""Defines an inherited list class MyList."""
 
 
 class MyList(list):
-    """implement sorted printing for the builtin list class."""
+    """Implements sorted printing for the built-in list class."""
 
     def print_sorted(self):
-        """Print a list in sorted assending order"""
+        """Print a list in sorted ascending order."""
         print(sorted(self))
