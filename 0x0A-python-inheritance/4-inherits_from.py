8bbdf71b510548d91bcc744aee5cb1fe71a6d4d4
Johnmarare <mararejohn@gmail.com>
2023-07-10 13:32:15 +0000

    Fix: ensuring objs type is not identical

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -12,6 +12,7 @@ def inherits_from(obj, a_class):
     Return:
         True if is instance
         else False.
-        #return issubclass(type(obj), a_class)
     """
-    return issubclass(type(obj), a_class)
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
