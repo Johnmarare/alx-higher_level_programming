d3bb8bcc6d2907cb377bb933a0e793710005282e
Johnmarare <mararejohn@gmail.com>
2023-07-10 13:41:11 +0000

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 6-base_geometry.py
+"""Improve Geometry"""
+
+
+class BaseGeometry:
+    """Improve Geometry"""
+
+    def __init__(self):
+        """Initializes instance"""
+        pass
+
+    def area(self):
+        """Ïmprove geometry"""
+        raise Exception("area() is not implemented")
