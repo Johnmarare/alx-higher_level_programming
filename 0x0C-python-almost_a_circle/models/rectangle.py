c0418ae542fa3af05af79f088f88efe72b900ed3
Johnmarare <mararejohn@gmail.com>
2023-08-04 12:23:06 +0000

    string representation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -88,3 +88,8 @@ class Rectangle(Base):
         """prints in stdout the Rectangle instance with charcter #"""
         for _ in range(self.__height):
             print("#" * self.__width)
+
+    def __str__(self):
+        """"custom string representation for Rectangle"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.__x, self.__y, self.__width, self.__height)
