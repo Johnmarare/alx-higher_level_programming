5a22f805d19f1e243887c6dba6a5915442475aa3
Johnmarare <mararejohn@gmail.com>
2023-08-17 21:23:58 +0000

    task 1 fix

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,3 @@
--- Creates the MySQL server user user_0d_1
-DROP USER IF EXISTS 'user_0d_1'@'localhost';
+-- creates the MySQL server user
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1' @'localhost';
