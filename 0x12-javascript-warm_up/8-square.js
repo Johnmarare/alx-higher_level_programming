528de1024ac3e8ee0ab50913ee410f9319b973ac
Johnmarare <mararejohn@gmail.com>
2023-09-12 18:45:56 +0000

    square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+// print square
+const args = process.argv[2];
+
+if (isNaN(args)) {
+  console.log('Missing size');
+} else {
+  let i = 0;
+  while (i < args) {
+    let j = 0;
+    let row = '';
+    while (j < args) {
+      row += 'x';
+      j++;
+    }
+    console.log(row);
+    i++;
+  }
+}
