c90c767dd3976edebb6f5399f09008bf50d88f03
Johnmarare <mararejohn@gmail.com>
2023-09-12 15:44:52 +0000

    fix, removed length

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // print first argument passed to it.
 
-if (process.argv.length <= 2) {
-  console.log('No argument');
-} else {
+if (process.argv[2]) {
   console.log(process.argv[2]);
+} else {
+  console.log('No argument');
 }
