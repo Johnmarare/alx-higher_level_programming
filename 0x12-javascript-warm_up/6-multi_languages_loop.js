c7faee5594f3b91ce0f448a1b508e7c18b714379
Johnmarare <mararejohn@gmail.com>
2023-09-12 17:58:51 +0000

    fix, spellcheck

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,7 +2,7 @@
 // loop
 const first = 'C is fun';
 const sec = 'Python is cool';
-const third = 'JavaScript is awesome';
+const third = 'JavaScript is amazing';
 
 const arr = [first, sec, third];
 
