4ee41478367d71ac7042f5a2038485a3a731bd9e
Johnmarare <mararejohn@gmail.com>
2023-09-12 17:57:16 +0000

    loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// loop
+const first = 'C is fun';
+const sec = 'Python is cool';
+const third = 'JavaScript is awesome';
+
+const arr = [first, sec, third];
+
+let i = 0;
+while (i < 3) {
+  console.log(arr[i]);
+  i++;
+}
