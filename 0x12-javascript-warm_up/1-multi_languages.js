124397d7c4474453cb6d477cacf70cf977959ff7
Johnmarare <mararejohn@gmail.com>
2023-09-12 14:10:09 +0000

    print 3 lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/*
+ * script that prints 3 lines
+ */
+const first = 'C is fun';
+const second = 'Python is cool';
+const third = 'Javascript is amazing';
+
+console.log(first);
+console.log(second);
+console.log(third);
