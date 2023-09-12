d03baac6780d3503321974f1c7b2b81262ceb70d
Johnmarare <mararejohn@gmail.com>
2023-09-12 14:54:54 +0000

    Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/*
+ * process.argv
+ */
+const args = process.argv.length - 2;
+
+if (args === 0) {
+  console.log('No argument');
+} else if (args === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Argument found');
+}
