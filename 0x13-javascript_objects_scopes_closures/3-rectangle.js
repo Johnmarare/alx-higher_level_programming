46c3b19a8bb88778191a84b75ec57c77f1ef99a0
Johnmarare <mararejohn@gmail.com>
2023-09-13 19:01:33 +0000

    print method

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  // instance method
+  print () {
+    let i = 0;
+    while (i < this.height) {
+      let j = 0;
+      let row = '';
+      while (j < this.width) {
+        row += 'X';
+        j++;
+      }
+      console.log(row);
+      i++;
+    }
+  }
+};
