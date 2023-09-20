2b8139e542f849b43fca083c88915fbfb9820a2d
Johnmarare <mararejohn@gmail.com>
2023-09-20 08:45:09 +0000

    fix: tostring

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,12 +1,5 @@
 #!/usr/bin/node
 
 exports.converter = function (base) {
-  // helper function to perform conversion recursively
-  const convertRecurse = (number, base) =>
-    number < base
-      ? number.toString()
-      : convertRecurse(Math.floor(number / base), base) + (number % base);
-
-  // return the conveter function
-  return (number) => convertRecurse(number, base);
+  return num => num.toString(base);
 };
