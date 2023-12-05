7428281ca7e58fa20617e35960497f56a7e70562
Johnmarare <mararejohn@gmail.com>
2023-12-05 14:02:38 +0000

    Error Code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+"""
+script that takes in a URL, sends a request to the URL
+and displays the body of the response
+"""
+import urllib.request  # for making http requests
+import urllib.error  # for status error codes
+import sys  # for command line arguments
+
+if __name__ == "__main__":
+    url = sys.argv[1]  # url from command line
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read().decode('utf-8')
+            print(body)
+    except urllib.error.HTTPError as e:
+        # handling HTTP error exception
+        print("Error code:", e.code)
